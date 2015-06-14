a84af6b6a1a60ede4989c760bbd62a6668655ac9
Lehel Iszlai <iszlai.lehel@gmail.com>
2015-06-14 10:30:01 +0200

    Fixed caching

--- analytics/src/main/resources/static/controllers.js
+++ analytics/src/main/resources/static/controllers.js
@@ -7,20 +7,21 @@ var redditScraperControllers = angular.module('redditScraperControllers', []);
 redditScraperControllers.controller('TodoListController', [ '$scope', '$http',
 		function($scope, $http) {
 
-			console.log("running controller");
-			$scope.todos = sessionStorage.getItem("scrapeData");
-			console.log("storage data[" + JSON.stringify($scope.todos) + "]");
+			console.log("running TodoListController");
+			$scope.todos = JSON.parse(sessionStorage.getItem("scrapeData"));
+			if($scope.todos!=null){
+				console.log("storage data[" + JSON.stringify($scope.todos.length) + "]");
+			}
+			else {
+				console.log("scope.todo null")
+			}
 			$scope.filterText = function(a) {
 				$scope.searchText = a;
 			}
 
-			if (angular.isUndefined($scope.todos) || $scope.todos === null) {
-				$http.get('/posts').success(function(data) {
-					console.log("fetched data");
-					$scope.todos = data;
-					sessionStorage.setItem("scrapeData", JSON.stringify(data));
-					console.log("scrapeData set again");
-					$scope.typeCounts = data.map(function(d) {
+			function doGroupBy(post){
+				if(post!=null){
+				return post.map(function(d) {
 						return d.src
 					}).reduce(function(acc, curr) {
 						if (typeof acc[curr] == 'undefined') {
@@ -30,11 +31,24 @@ redditScraperControllers.controller('TodoListController', [ '$scope', '$http',
 						}
 
 						return acc;
-					}, {});
+					}, {});}else {
+						console.log("groupby null")
+					}
+			}
+
+			if (angular.isUndefined($scope.todos) || $scope.todos === null) {
+				$http.get('/posts').success(function(data) {
+					console.log("fetched data");
+					$scope.todos = data;
+					sessionStorage.setItem("scrapeData", JSON.stringify(data));
+					console.log("scrapeData set again" +JSON.stringify(data.length));
+					$scope.typeCounts = doGroupBy(data);
 
 					console.log($scope.typeCounts)
 				});
 			}
+
+			$scope.typeCounts = doGroupBy($scope.todos);
 		} ]);
 
 redditScraperControllers.controller('VotesController', [ '$scope', '$http',
